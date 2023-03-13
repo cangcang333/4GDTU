0b67d6023e4c3e6cf3baece59e826cb141509455
Danial <cangcang333@163.com>
2023-03-13 12:22:22 +0800

    add test entity

---
+++ src/main/java/com/spzhu/entity/base/learning/Group.java
@@ -0,0 +1,10 @@
+package com.spzhu.entity.base.learning;
+
+public class Group {
+    public int gradNo;
+    public int classNo;
+    public Group(int gradNo, int classNo) {
+        this.gradNo = gradNo;
+        this.classNo = classNo;
+    }
+}
---
+++ src/main/java/com/spzhu/entity/base/learning/Student.java
@@ -0,0 +1,12 @@
+package com.spzhu.entity.base.learning;
+
+public class Student {
+    public int id;
+    public int age;
+    public Group group;
+
+    public Student(int id, int age) {
+        this.id = id;
+        this.age = age;
+    }
+}
